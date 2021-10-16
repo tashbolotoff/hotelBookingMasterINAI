8f324d23d0a077a4d3b99ee22f37b230b8bc136c
tashbolotoff <b.tashbolotoff@gmail.com>
2021-10-16 17:53:08 +0600

    created Spring Boot project and added all start files

---
+++ src/test/java/kg/inaiproject/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.inaiproject.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
