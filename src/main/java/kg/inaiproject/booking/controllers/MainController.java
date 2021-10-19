8cc04506afce1e238f87aef738d099fc0e207c2c
tashbolotoff <b.tashbolotoff@gmail.com>
2021-10-19 20:38:49 +0600

    edited some files

--- src/main/java/kg/inaiproject/booking/controllers/MainController.java
+++ src/main/java/kg/inaiproject/booking/controllers/MainController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-@RequestMapping("/")
 public class MainController {
 
-    @GetMapping
-    public String getIndex() {
-        return "index";
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        if (request.isUserInRole("ROLE_ADMIN")) {
+            return "redirect:/admin/user/list";
+        } else {
+            return "redirect:/booking/list";
+        }
     }
 }
